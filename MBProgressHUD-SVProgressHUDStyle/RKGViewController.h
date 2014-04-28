4e108fdedcc7fa2d5f1ea2d30b864ec5bc9df5ff
Leon <phaibin@gmail.com>
2014-04-28 16:07:26 +0800

    Initial Commit

---
+++ MBProgressHUD-SVProgressHUDStyle/RKGViewController.h
@@ -0,0 +1,13 @@
+//
+//  RKGViewController.h
+//  MBProgressHUD-SVProgressHUDStyle
+//
+//  Created by Leon on 4/28/14.
+//  Copyright (c) 2014 Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RKGViewController : UIViewController
+
+@end
---
+++ MBProgressHUD-SVProgressHUDStyle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MBProgressHUD-SVProgressHUDStyle
+//
+//  Created by Leon on 4/28/14.
+//  Copyright (c) 2014 Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RKGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RKGAppDelegate class]));
+    }
+}
